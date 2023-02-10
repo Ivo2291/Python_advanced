f3ee7cfa49af5661c31a7622cdbbea65c2df7870
Ivo Todorov <ivo2291@gmail.com>
2023-02-10 17:37:12 +0200

    Add files via upload

---
+++ 03. multidimensional_lists/sum_matrix_columns.py
@@ -0,0 +1,12 @@
+rows, columns = [int(x) for x in input().split(', ')]
+matrix = []
+
+for i in range(rows):
+    matrix.append([int(x) for x in input().split()])
+
+for column in range(columns):
+    result = 0
+    for row in range(rows):
+        result += matrix[row][column]
+
+    print(result)
