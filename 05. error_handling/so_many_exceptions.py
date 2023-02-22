a1da6ee228d14d5bb7e47f73e18dbd3845784f01
Ivo Todorov <ivo2291@gmail.com>
2023-02-22 16:44:55 +0200

    Add files via upload

---
+++ 05. error_handling/so_many_exceptions.py
@@ -0,0 +1,25 @@
+# This code raises many exceptions. Fix it, so it works correctly
+
+# numbers_list = input().split(", ")
+# result = 0
+#
+# for i in range(numbers_list):
+#     number = numbers_list[i + 1]
+#     if number < 5:
+#         result *= number
+#     elif number > 5 and number > 10:
+#         result /= number
+#
+# print(result)
+
+
+numbers_list = list(map(int, input().split(", ")))
+result = 1
+
+for number in numbers_list:
+    if number <= 5:
+        result *= number
+    elif number <= 10:
+        result /= number
+
+print(result)
